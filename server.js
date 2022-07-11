85fb1b48e5b42e8674ddec588200152cf3a03a3f
kyrodevs <108683799+kyrodevs@users.noreply.github.com>
2022-07-11 20:36:09 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 
+const importData = require("./data.json");
+
 const port = process.env.PORT || 3000;
 
 app.get("/", function (req,res) {
     res.send("Leonardo Prado Cosenza");
+});
+
+app.get("/produtos", function(req, res) {
+    res.send(importData);
 });
 
 app.listen(port, () => {
